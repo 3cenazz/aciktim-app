4aa8a0ab2d90105748a75fdabafa0baa0fcce721
3cenaz <eegri21@ku.edu.tr>
2023-08-15 16:27:00 +0300

    Deleting
    Deleting wrong code

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ import reportWebVitals from './reportWebVitals';
 
 import { Provider } from 'react-redux';
 import {store} from "./redux/store"
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"></link>
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
