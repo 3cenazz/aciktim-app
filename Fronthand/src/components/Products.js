8566d853e78244366285f845245fcb2963d6f3b2
3cenaz <eegri21@ku.edu.tr>
2023-09-06 17:22:44 +0300

    Added Properties
    Number and description is added for products

--- Fronthand/src/components/Products.js
+++ Fronthand/src/components/Products.js
@@ -15,82 +15,106 @@ const menuItems = [
         id: 1,
         name: "Menu 1",
         price: 80.5,
-        image: one
+        image: one,
+        number: 1,
+        description: "Delicious hamburger served with fresh vegetables and special sauce.",
     },
     {
         id: 2,
         name: "Menü 2",
-        price: 99.90,
-        image: two
+        price: 99.9,
+        image: two,
+        number: 1,
+        description: "A delightful hamburger made with prime beef and cheddar cheese.",
     },
    
     {
         id: 3,
         name: "Menü 3",
-        price: 110.90,
-        image: three
+        price: 110.9,
+        image: three,
+        number: 1,
+        description: "Vegan option: A healthy and tasty plant-based hamburger.",
     },
    
     {
         id: 4,
         name: "Menü 4",
         price: 100,
-        image: four
+        image: four,
+        number: 1,
+        description: "Savor the classic taste of our hamburger, featuring a juicy beef patty, pickles, and American cheese on a toasted bun.",
     },
    
     {
         id: 5,
         name: "Menü 5",
         price: 165,
-        image: five
+        image: five,
+        number: 1,
+        description: "For the ultimate burger experience, feast on our signature double cheeseburger, featuring two beef patties, bacon, and all the fixings.",
     },
     {
         id: 6,
         name: "Menü 6",
-        price: 149.90,
-        image: six
+        price: 149.9,
+        image: six,
+        number: 1,
+        description: "A mouthwatering burger with a twist! Our spicy jalapeño burger is sure to satisfy your cravings with its fiery kick.",
     },
    
     {
         id: 7,
         name: "Menü 7",
-        price: 139.90,
-        image: seven
+        price: 139.75,
+        image: seven,
+        number: 1,
+        description: "Treat yourself to a gourmet burger experience with our truffle-infused burger topped with creamy brie cheese and caramelized onions.",
     },
    
     {
         id: 8,
         name: "Menü 8",
         price: 80.5,
-        image: eight
+        image: eight,
+        number: 1,
+        description: "A classic chicken burger featuring a tender grilled chicken breast, fresh lettuce, and tangy honey mustard dressing.",
     },
    
     {
         id: 9,
         name: "Menü 9",
-        price: 59.90,
-        image: nine
+        price: 59.99,
+        image: nine,
+        number: 1,
+        description: "Our BBQ burger is a flavor explosion with its succulent pulled pork, coleslaw, and BBQ sauce. A true BBQ lover's delight.",
     },
    
     {
         id: 10,
         name: "Menü 10",
-        price: 99.90,
-        image: ten
+        price: 99.99,
+        image: ten,
+        number: 1,
+        description: "Indulge in our bacon and avocado burger, a savory masterpiece featuring crispy bacon and creamy avocado slices.",
     },
    
     {
         id: 11,
         name: "Menü 11",
-        price: 115.90,
-        image: eleven
+        price: 115.99,
+        image: eleven,
+        number: 1,
+        description: "For a taste of Italy, try our Caprese burger topped with fresh mozzarella, ripe tomatoes, basil, and balsamic glaze.",
     },
    
     {
         id: 12,
         name: "Menü 12",
-        price: 79.90,
-        image: twelve
+        price: 79.5,
+        image: twelve,
+        number: 1,
+        description: "Experience the flavors of the Southwest with our spicy Tex-Mex burger, loaded with jalapeños, guacamole, and salsa.",
     },
    
 ]
